00dad85ae4a4b56ecdef865f2dc12c5581aea4b6
Pratik <pratikshaw8@gmail.com>
2024-02-25 17:57:29 +0530

    Set up view engine

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const port = 8000;
 //use express router for the routers folder we created(middleware)
 app.use('/',require('./routes/index'));
 
+//set up view engine as ejs
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
+
+
+
+//defining localhost port
 app.listen(port,(err)=>{
     if(err)console.log(`Error listening on port: ${err}`);
     else console.log(`Listening on port: ${port}`);
